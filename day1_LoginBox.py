09f2ef5801fb1c7f6f03aa35d70e82688f8cb78b
INGLEE <liruijia_123@126.com>
2018-07-06 17:56:03 +0800

    tk学习
    更新登录组件

--- day1_LoginBox.py
+++ day1_LoginBox.py
@@ -67,10 +67,25 @@ entry2=Entry(root,textvariable=u,show='*')
 entry2.bind('<Button-1>',cls2)
 entry2.grid(row=1,column=1,sticky=E)
 def callback():
-	global u
-	print(u.get())
+	t1 = entry1.get()
+	t2 = entry2.get()
+	t3=StringVar()
+	if (t1 == "admin") & (t2 == "admin"):
+		t3.set("登录成功")
+	else:
+		t3.set("登录失败")
+		entry1.delete(0,len(t1))
+		entry2.delete(0,len(t2))
+	Label(root,textvariable=t3).grid(row=3,column=0,sticky=EW)
+	
 
 Button(root,text="登录",command=callback).grid(row=2,column=1,sticky=EW)
+
 root.mainloop()
 
-
+'''
+e.delete()删除内容
+-- 删除参数 first 到 last 范围内（包含 first 和 last）的所有内容
+-- 如果忽略 last 参数，表示删除 first 参数指定的选项
+-- 使用 delete(0, END) 实现删除输入框的所有内容
+'''
